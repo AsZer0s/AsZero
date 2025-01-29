700c49b1659a9e5d789525fd29268f9ca4e3a2b8
AsZer0s <AsZer0s@users.noreply.github.com>
2025-01-29 12:30:25 +0000

    deploy: aa0bbdaba7c3a4289c0891ee35d3d79b272ebb45

---
+++ music/service-worker.js
@@ -0,0 +1,1 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");importScripts("precache-manifest.d66adcdfbd358d976f77b26675cd1a60.js");workbox.core.setCacheNameDetails({prefix:"unlock-music"});workbox.core.skipWaiting();self.__precacheManifest=[].concat(self.__precacheManifest||[]);workbox.precaching.precacheAndRoute(self.__precacheManifest,{});
